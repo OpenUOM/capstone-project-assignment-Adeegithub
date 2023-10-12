d1ea9b050e100301726cc8be141b6e5fba7f7dbd
Adeegithub <arunodagunawardana@gmail.com>
2023-10-12 09:54:02 +0530

    "Fix2"

--- frontend/test/teacher-edit.test.js
+++ frontend/test/teacher-edit.test.js
@@ -4,28 +4,21 @@ process.env.NODE_ENV = "test";
 fixture`Testing Teacher UI`
     .page`http://localhost:4401/`
 test('Testing edit teachers', async t => {
-    await t.navigateTo("/addTeacher");
-    await t.typeText("#teacher-id", "123456");
-    await t.typeText("#teacher-name", "Mohan Perera");
-    await t.typeText("#teacher-age", "45");
-    await t.click("#teacher-add");
-
-    await t.navigateTo("/teacher");
-    await t.click("#teacher-edit-123456");
+    await t.navigateTo("/");
+    await t.click("#teacher-edit-10003");
 
     await t.typeText("#teacher-name", "Changed Teacher Name");
     await t.typeText("#teacher-age", "99");
     await t.click("#teacher-edit");
 
-    await t.navigateTo("/teacher");
+    await t.navigateTo("/");
 
     const table = Selector('#teacher-table')
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    console.log("******** ACTUAL TEXT ********" ,tdText);
+    console.log("******* Added Teacher *******" + tdText)
     await t.expect(tdText).contains("Changed Teacher Name");
 
-    await t.navigateTo("/teacher");
-    await t.click("#teacher-delete-123456");
+    await t.click("#teacher-delete-10003");
 });
