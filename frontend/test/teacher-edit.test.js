aff8e8714f308a6e431062440464befd8a403fc1
Adeegithub <arunodagunawardana@gmail.com>
2023-10-19 11:54:44 +0530

    added await for 1 second

--- frontend/test/teacher-edit.test.js
+++ frontend/test/teacher-edit.test.js
@@ -16,6 +16,8 @@ test('Testing edit teachers', async t => {
     const table = Selector('#teacher-table')
     const rowCount = await table.find('tr').count;
 
+    await t.wait(1000);
+
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
     await t.expect(tdText).contains("Changed");
 
